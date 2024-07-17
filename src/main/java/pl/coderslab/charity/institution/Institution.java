fa058ff70f3cf4bc4cfc689b37ffbbd1c6159da2
TarczynskiGrzegorz <tarczynski.tg@gmail.com>
2024-07-17 18:54:59 +0200

    entity-institution

---
+++ src/main/java/pl/coderslab/charity/institution/Institution.java
@@ -0,0 +1,20 @@
+package pl.coderslab.charity.institution;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Institution {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 60)
+    private String name;
+    @Column(length = 200)
+    private String description;
+}
